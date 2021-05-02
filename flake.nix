0325c421862c47f4a7dbbcc9ee1b7eab854b5ef3
Yannik Sander <me@ysndr.de>
2021-05-03 01:16:32 +0200

    Include a nixos configuration in flake

--- flake.nix
+++ flake.nix
@@ -19,15 +19,64 @@
     let
       overlay = import ./overlay.nix { inherit inventaire-client-src inventaire-server-src inventaire-i18n-src; };
     in
-      {
+      rec {
         inherit overlay;
+        nixosModule = import ./module.nix;
+        nixosConfigurations."container" = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules =
+            [
+              (
+                { pkgs, ... }: {
+                  imports = [
+                    nixosModule
+                  ];
+                  nixpkgs.overlays = [ overlay ];
+
+                  boot.isContainer = true;
+
+                  # Let 'nixos-version --json' know about the Git revision
+                  # of this flake.
+                  system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+
+                  # Network configuration.
+                  networking.useDHCP = false;
+                  networking.firewall.allowedTCPPorts = [ 80 3006 ];
+
+
+                  # Enable the inventaire server.
+                  services.inventaire = {
+                    enable = true;
+                    config = builtins.readFile ./test/local.js;
+                  };
+
+                  # dependency services
+                  services.couchdb = {
+                    enable = true;
+                    extraConfig = ''
+                      [admins]
+                      yourcouchdbusername=yourcouchdbpassword
+                    '';
+                    package = pkgs.couchdb2;
+
+                  };
+                  services.nginx.enable = true;
+                  services.elasticsearch.enable = true;
+                  services.elasticsearch.package=pkgs.elasticsearch7;
+
+                  nixpkgs.config.allowUnfree = true;
+                }
+              )
+            ];
+        };
       } // flake-utils.lib.eachSystem [ "x86_64-linux" ] (
         system:
           let
             pkgs = import nixpkgs { inherit system overlay; };
           in
-            {
-              nixosModule = import ./module.nix;
+            rec {
+
+              apps.update-deps = flake-utils.lib.mkApp { drv = pkgs.writeScriptBin "update-deps" (builtins.readFile ./scripts/update.sh); };
             }
       );
 }
