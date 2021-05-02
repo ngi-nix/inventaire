d638a09fe8c9dea7fdf5c19670ed96c0f3fbf3e7
Yannik Sander <me@ysndr.de>
2021-05-02 23:28:53 +0200

    nixfmt on flake.nix

--- flake.nix
+++ flake.nix
@@ -3,31 +3,31 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.inventaire-client-src = {
-      url = "github:inventaire/inventaire-client";
-        flake=false;
+    url = "github:inventaire/inventaire-client";
+    flake = false;
   };
   inputs.inventaire-server-src = {
-      url = "github:inventaire/inventaire";
-      flake= false;
+    url = "github:inventaire/inventaire";
+    flake = false;
   };
   inputs.inventaire-i18n-src = {
-      url = "github:inventaire/inventaire-i18n/dist";
-      flake= false;
+    url = "github:inventaire/inventaire-i18n/dist";
+    flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils, inventaire-client-src, inventaire-server-src, inventaire-i18n-src}:
-       let
-        overlay = import ./overlay.nix { inherit inventaire-client-src inventaire-server-src inventaire-i18n-src; };
-        in
-    {
-        inherit overlay;
-    } //
-    flake-utils.lib.eachSystem ["x86_64-linux"] ( system:
+  outputs = { self, nixpkgs, flake-utils, inventaire-client-src, inventaire-server-src, inventaire-i18n-src }:
     let
-        pkgs = import nixpkgs { inherit system overlay; };
+      overlay = import ./overlay.nix { inherit inventaire-client-src inventaire-server-src inventaire-i18n-src; };
     in
-    {
-        nixosModule = import ./module.nix;
-    }
-    );
+      {
+        inherit overlay;
+      } // flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+        system:
+          let
+            pkgs = import nixpkgs { inherit system overlay; };
+          in
+            {
+              nixosModule = import ./module.nix;
+            }
+      );
 }
--- overlay.nix
+++ overlay.nix
@@ -4,26 +4,6 @@ let
   overlay = pkgs:
 
     let
-
-      # inventaire-client-src = pkgs.fetchFromGitHub {
-      #   owner = "inventaire";
-      #   repo = "inventaire-client";
-      #   rev = "954cb91ccdec9557ec5be8b53dead6971fd0984f";
-      #   sha256 = "sha256:1v575lhx5rg8nny3lkwcjb40nb0wrzdkizwirfr1y0fbfccjhxw8";
-      # };
-
-      # inventaire-server-src = pkgs.fetchFromGitHub {
-      #   owner = "inventaire";
-      #   repo = "inventaire";
-      #   rev = "a8928931c4f749751702f734f19f0e620a9d675b";
-      #   sha256 = "sha256:0nxq9k1d9y6a59jsva7nj0mmivrxxyn09v6v32axhrzgb6jwksnr";
-      # };
-
-      # inventaire-i18n-src = builtins.fetchGit {
-      #   url = "https://github.com/inventaire/inventaire-i18n.git";
-      #   ref = "dist";
-      # };
-
       piwik-js = builtins.fetchurl {
         url = https://piwik.allmende.io/piwik.js;
         sha256 = "sha256:1gsfhry3z9qwc17q68qhdf9ihrmqd20qya4694f1dpqg214i1baj";
